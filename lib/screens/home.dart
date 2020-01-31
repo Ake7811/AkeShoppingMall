2443a8de3c1e9e2dbdd298508acc7313b98aa683
Ake7811 <eak.noppadol@gmail.com>
2020-01-31 18:39:26 +0700

    Material Pageroute

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:akeshoppingmall/screens/register.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -17,22 +18,78 @@ class _HomeState extends State<Home> {
   }
 
   Widget showAppName() {
-    return Text('Ake Shopping Mall');
+    return Text(
+      'Ake Shopping Mall',
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.blue.shade700,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        fontFamily: 'PTSans',
+      ),
+    );
+  }
+
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.blue.shade700,
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      child: Text('Sign Up'),
+      onPressed: () {
+        print('You Click Sign Up');
+        MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (BuildContext context) => Register());
+        Navigator.of(context).push(materialPageRoute);
+
+      },
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        SizedBox(
+          width: 4.0,
+        ),
+        signUpButton()
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-            child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            showLogo(),
-            showAppName(),
-            showAppName(),
-          ],
-        )),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, Colors.yellow.shade800],
+              radius: 1.0,
+            ),
+          ),
+          child: Center(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showLogo(),
+              showAppName(),
+              SizedBox(
+                height: 8.0,
+              ),
+              showButton(),
+            ],
+          )),
+        ),
       ),
     );
   }
